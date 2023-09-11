5b1ace75c05d219d9ee97bf777c0f2b0d6575c9c
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2023-09-11 15:38:12 +0000

    initial commit

---
+++ week 2/week2.js
@@ -0,0 +1,16 @@
+// Array of names
+const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+// Loop through the array and print each name
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+// Basic conditional statement
+const age = 25;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
---
+++ week 3/index.js
@@ -0,0 +1,13 @@
+function addThreeNumbers(num1, num2, num3) {
+    var result = num1 + num2 + num3;
+    console.log("The sum of", num1, "+", num2, "+", num3, "is:", result);
+    return result;
+}
+
+// Input values
+var number1 = 5;
+var number2 = 10;
+var number3 = 15;
+
+// Call the function
+var sumResult = addThreeNumbers(number1, number2, number3);
